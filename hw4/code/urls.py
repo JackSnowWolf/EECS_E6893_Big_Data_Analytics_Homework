2d3c62957432258b30ce1e304d7860c0c5c54df4
huchong <1178869226@qq.com>
2019-11-08 18:35:25 -0500

    [INIT] init hw4

---
+++ hw4/code/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.urls import path
+from . import view
+ 
+urlpatterns = [
+    path('hello', view.hello),
+    path('dashboard', view.dashboard),
+    path('connection', view.connection)
+]
---
+++ hw4/code/view.py
@@ -0,0 +1,66 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import pandas_gbq
+from google.oauth2 import service_account
+
+# Make sure you have installed pandas-gbq at first;
+# You can use the other way to query BigQuery.
+# please have a look at
+# https://cloud.google.com/bigquery/docs/reference/libraries#client-libraries-install-nodejs
+# To get your credential
+
+credentials = service_account.Credentials.from_service_account_file('xx/xx/xxxx.json')
+
+
+def hello(request):
+    context = {}
+    context['content1'] = 'Hello World!'
+    return render(request, 'helloworld.html', context)
+
+
+def dashboard(request):
+    pandas_gbq.context.credentials = credentials
+    pandas_gbq.context.project = "Your-Project"
+
+    SQL = ""
+    df = pandas_gbq.read_gbq(SQL)
+
+    data = {}
+
+    '''
+        TODO: Finish the SQL to query the data, it should be limited to 8 rows. 
+        Then process them to format below:
+        Format of data:
+        {'data': [{'Time': hour:min, 'count': {'ai': xxx, 'data': xxx, 'good': xxx, 'movie': xxx, 'spark': xxx}},
+                  {'Time': hour:min, 'count': {'ai': xxx, 'data': xxx, 'good': xxx, 'movie': xxx, 'spark': xxx}},
+                  ...
+                  ]
+        }
+    '''
+
+    return render(request, 'dashboard.html', data)
+
+
+def connection(request):
+    pandas_gbq.context.credentials = credentials
+    pandas_gbq.context.project = "Your-Project"
+    SQL1 = ''
+    df1 = pandas_gbq.read_gbq(SQL1)
+
+    SQL2 = ''
+    df2 = pandas_gbq.read_gbq(SQL2)
+
+    data = {}
+
+    '''
+        TODO: Finish the SQL to query the data, it should be limited to 8 rows. 
+        Then process them to format below:
+        Format of data:
+        {'n': [xxx, xxx, xxx, xxx],
+         'e': [{'source': xxx, 'target': xxx},
+                {'source': xxx, 'target': xxx},
+                ...
+                ]
+        }
+    '''
+    return render(request, 'connection.html', data)
